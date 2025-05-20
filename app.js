ed0a6df0d639e31f836fc21df016cb20c18c3beb
NarendraSingh197 <narendra.dev@thesukrut.com>
2025-05-20 06:38:26 -0400

    add swagger autogen

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ dotenv.config()
 const express=require("express")
 const cors =require("cors")
 const cookieParser = require('cookie-parser');
+const swaggerUi  = require("swagger-ui-express"); 
+const swaggerDocs = require("./swagger-output.json"); // Import the generated Swagger JSON
 
 const app=express()
 const connectToDB=require("./db")
@@ -11,9 +13,14 @@ app.use(cors())
 app.use(cookieParser());
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+// Swagger UI
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs)); // Serve Swagger UI
 const userRouter=require("./routers/userRouters")
+const riderRouter=require("./routers/riderRouters")
 const authMiddleware = require("./middelwares/AuthMiddelware")
 app.use("/api/user",userRouter)
+app.use("/api/rider",riderRouter)
 
 
 
